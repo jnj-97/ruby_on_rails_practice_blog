9e339f031e9552baa259adb4994bf5c4d788111e
jnj-97 <nobinjohnson3@gmail.com>
2025-12-02 23:36:24 +0530

    Beginner Ruby on Rails blogs app

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,43 @@
+class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+    def new
+        @article = Article.new
+    end
+    def show
+        @article = Article.find(params[:id])
+    end
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def delete
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to articles_path
+        else
+            render :edit,status: :unprocessable_entity
+        end
+    end
+    
+    def create
+    @article=Article.new(article_params)
+    if @article.save
+        redirect_to articles_path
+    else
+        render :new,status: :unprocessable_entity
+    end
+end
+
+    private 
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end 
+end
